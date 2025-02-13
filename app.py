2b9338d06150c569f214d6270cf8986e0cf54f68
Joseanpm <148182770+Joseanpm@users.noreply.github.com>
2025-02-13 13:42:18 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -11,5 +11,6 @@ def home():
 def descargar_carta():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'carta_san_valentin.pdf', as_attachment=True)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render asigna el puerto automáticamente
+    app.run(host="0.0.0.0", port=port, debug=False)  # Asegúrate de que debug esté en False
